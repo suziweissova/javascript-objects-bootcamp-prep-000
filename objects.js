fe98f2704e8a497e9ee77aad60783eddb992b9d8
Susann Weissova <suzi.weissova@gmail.com>
2018-06-21 08:30:15 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = {
-  'Foo Fighters': 'Everlong',
-  'Prince': 'Purple Rain'
+  FooFighters: 'Everlong',
+  Prince: 'Purple Rain'
 }
 
 function updatePlaylist(playlist, artist, song) {
