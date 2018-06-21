1940016d1374368696bc23be564505e88bcb9dad
Susann Weissova <suzi.weissova@gmail.com>
2018-06-21 08:27:34 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,17 +4,11 @@ var playlist = {
 }
 
 function updatePlaylist(playlist, artist, song) {
-  // Remember, because one of this function's arguments
-  // is `playlist`, the `playlist` _inside_ the function
-  // is not the same as the top-level `playlist` outside
-  // the function
   playlist[artist] = song
-
   return playlist
 }
 
 function removeFromPlaylist(playlist, artist) {
   delete playlist[artist]
-
   return playlist
 }
